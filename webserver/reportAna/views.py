dcf4dd2814e5ad9e3ce53573f3340d46ce1ae31d
astwyg <astwyg@126.com>
2020-03-08 10:27:27 +0800

    finish annual report keyword search

--- webserver/reportAna/views.py
+++ webserver/reportAna/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from reportAna.src.find_keywords import find_keyword
+
+def index(req):
+    context = {}
+    if req.method == "POST":
+        return HttpResponse("整啥呢? 不允许POST")
+    else:
+        keywords = req.GET.get("keywords")
+        path = req.GET.get("path")
+        if keywords and path:
+            msg = find_keyword(keywords, path)
+            context["msg"] = msg
+        return render(req, 'reportAna/find_keywords.html', context)
